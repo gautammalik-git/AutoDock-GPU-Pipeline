8090696c995becee29c81835addbd7b19943561d
Gautam <68741441+gautammalik-git@users.noreply.github.com>
2024-11-18 18:21:00 +0530

    Update gpu_hts.py

--- gpu_hts.py
+++ gpu_hts.py
@@ -19,9 +19,9 @@ if len(sys.argv) == 4:
         coord.append(z_cord)
         os.system("python gpu_filecr.py " + f'{protein} ' + ligand +str(coord[0])+","+str(coord[1])+","+str(coord[2])+ f' {nrun}')
 else:
-    print('''python3 gpu_hts.pdb <protein_pdb_file> <x,y,z> <nrun>
+    print('''python3 gpu_hts.py <protein_pdb_file> <x,y,z> <nrun>
 eg.
-python3 gpu_hts.pdb 7k15.pdb 1.9392312138728325,-8.92228901734104,18.28384393063584 10
+python3 gpu_hts.py 7k15.pdb 1.9392312138728325,-8.92228901734104,18.28384393063584 10
     ''')
 
 
